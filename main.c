0e56580dbea1d491f6f7e4d3025e89a675037ae6
btsutskiridze <93125681+btsutskiridze@users.noreply.github.com>
2022-06-11 02:40:54 +0400

    finished project
    added gameHistory function and modified random number generator using lfsr32 function and optimized as much as possible

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h> //for getch()
-#include <time.h> // for srand()
 
 /*************** TIC TAC TOE GAME ***************/
 
@@ -17,26 +16,24 @@ const char PLAYER2 = 'O';
 //menu part
 void startMenu(); //done
 void gameMode(); //done
-void gameRules(); //done
-void gameHistory(); // isn't started
+void gameHistory(); // done
 
-void playWithComputer(int); //easy part is done working on hard mode
+void playWithComputer(int); //done
 void playWithHuman(); //done
 //game structure
 void resetBoard(); //done
 void printBoard(); //done
 int checkSpaces(); //done
-void firstPlayerMove(); //done
-void secondPlayerMove(); //done
+void playerMove(char); //done
 void computerEasyMove(); // done
-void computerHardMove(); // doing now
+void computerHardMove(); // done????
 char checkWinner(); //done
 void printWinner(char); //done
-void print2PlayerWinner(char); //done
+void addHistory(char*,char*); //done
+unsigned long lfsr32(unsigned long seed);
 
-
+//driver
 int main(){
-  srand(time(0));
   printf("\n\n\t\t\tWELCOME!!\n");
   printf("\t\t\tTHIS IS A TIC TAC TOE GAME\n\n");
   startMenu();
@@ -49,9 +46,8 @@ void startMenu(){
   printf("\n\t\t\tChoose an option:\n");
   printf("\t\t\t______________________\n");
   printf("\t\t\t1. Start Game\n");
-  printf("\t\t\t2. Rules of the Game\n");
-  printf("\t\t\t3. Gameplay History\n");
-  printf("\t\t\t4. Quit the Game\n");
+  printf("\t\t\t2. Gameplay History\n");
+  printf("\t\t\t3. Quit the Game\n");
   printf("\t\t\t----------------------\n");
   while(1){
     printf("\t\t\t");
@@ -64,13 +60,10 @@ void startMenu(){
         break;
       case '2':
         system("cls");
-        gameRules();
-        break;
-      case '3':
-        system("cls");
         gameHistory();
         break;
-      case '4':
+      case '3':
+        remove("history.txt");
         exit(1);
       default:
         printf("Invalid option!! CHOOSE AGAIN\n");
@@ -115,21 +108,23 @@ void gameMode(){
   }
 }
 
-void gameRules(){
-  printf("\n\n\t\t1. The game is played on a grid \n\t\t that's 3 squares by 3 squares.\n\n");
-  printf("\t\t2. You are X, your friend (or the computer in this case) is O.\n\t\t Players take turns putting their marks in empty squares.\n\n");
-  printf("\t\t3. The first player to get 3 of her marks \n\t\t in a row (up, down, across, or diagonally) is the winner.\n\n");
-  printf("\t\t4. When all 9 squares are full, the game is over.\n\t\t If no player has 3 marks in a row, the game ends in a tie.\n\n");
-
-  printf("\t\tPRESS ANY KEY TO GO BACK TO THE MENU\n");
-  char press;
-  press = getch();
+void gameHistory(){
+  FILE *filePtr = fopen("history.txt", "r");
+  int counter = 0;
+  if (filePtr == NULL){
+        printf("Error opening the file history.txt\n");
+        return;
+  }
+  printf("\n\t\t\t\tGAME HISTORY\n");
+  printf("\t\t\t---------------------------\n\n");
+  char text[50];
+  while(fgets(text, 50, filePtr)){
+    printf("\t\t\tN%d. %s", ++counter,text);
+  } 
+  fclose(filePtr);
+  getch();
   system("cls");
   startMenu();
-}
-
-void gameHistory(){
-
 }
 
 void playWithComputer(int n){
@@ -141,7 +136,7 @@ void playWithComputer(int n){
       while(winner == ' ' && checkSpaces()!=0 ){
         system("cls");
         printBoard();
-        firstPlayerMove();
+        playerMove(PLAYER);
         winner = checkWinner();
         if(winner != ' ' || checkSpaces() == 0){
           break;
@@ -156,7 +151,8 @@ void playWithComputer(int n){
       system("cls");
       printBoard();
       printWinner(winner);
-
+      if(winner == PLAYER) addHistory("Player","Computer");
+      else addHistory("Computer","Player");
       printf("\n\t\t\tWould you like to continue the game?\n");
       printf("\n\t\t\t(Y - Play Again // N - Back to Menu)\n");
       printf("\n\t\t\t\t\t");
@@ -174,7 +170,7 @@ void playWithComputer(int n){
       while(winner == ' ' && checkSpaces()!=0 ){
         system("cls");
         printBoard();
-        firstPlayerMove();
+        playerMove(PLAYER);
         winner = checkWinner();
         if(winner != ' ' || checkSpaces() == 0){
           break;
@@ -189,8 +185,8 @@ void playWithComputer(int n){
       system("cls");
       printBoard();
       printWinner(winner);
-
-      printf("\n\t\t\t\tTHANKS FOR PLAYING :)\n");
+      if(winner == PLAYER) addHistory("Player","Computer");
+      else addHistory("Computer","Player");
       printf("\n\t\t\t\tWould you like to continue the game?\n");
       printf("\n\t\t\t\t (Y - Play Again // N - Back to Menu)\n");
       printf("\n\t\t\t\t\t\t");
@@ -210,14 +206,14 @@ void playWithHuman(){
     while(winner == ' ' && checkSpaces()!=0 ){
       system("cls");
       printBoard();
-      firstPlayerMove();
+      playerMove(PLAYER);
       winner = checkWinner();
       if(winner != ' ' || checkSpaces() == 0){
         break;
       }
       system("cls");
       printBoard();
-      secondPlayerMove();
+      playerMove(PLAYER2);
       winner = checkWinner();
       if(winner != ' ' || checkSpaces() == 0){
         break;
@@ -226,8 +222,9 @@ void playWithHuman(){
     }
     system("cls");
     printBoard();
-    print2PlayerWinner(winner);
-
+    printWinner(winner);  
+    if(winner == PLAYER) addHistory("Player 1","Player 2");
+    else addHistory("Player 2","Player 1");
     printf("\n\t\t\tWould you like to continue the game?\n");
     printf("\n\t\t\t(Y - Play Again // N - Back to Menu)\n");
     printf("\n\t\t\t\t\t");
@@ -238,7 +235,6 @@ void playWithHuman(){
   startMenu();
 
 }
-
 
 //structure part
 void resetBoard(){
@@ -274,9 +270,9 @@ int checkSpaces(){
   return freeSpaces;
 }
 
-void firstPlayerMove(){
+void playerMove(char player){
   int x,y;
-  printf("\n\t\t\t\tPLAYER X MOVE:\n\n");
+  printf("\n\t\t\t\tPLAYER %c MOVE:\n\n",player);
   do
   {
     printf("\t\t\t\tEnter row #(1-3): ");
@@ -289,42 +285,25 @@ void firstPlayerMove(){
       printf("\t\t\t\tInvalid Move!!\n");
     }
     else{
-      board[x][y] = PLAYER;
+      board[x][y] = player;
       break;
     }
   } while (board[x][y]!= ' ');
 
-}
-
-void secondPlayerMove(){
-  int x,y;
-  printf("\n\t\t\t\tPLAYER O MOVE:\n\n");
-  do
-  {
-    printf("\t\t\t\tEnter row #(1-3): ");
-    scanf("%d", &x);
-    x--;
-    printf("\t\t\t\tEnter column #(1-3): ");
-    scanf("%d", &y);
-    y--;
-    if(board[x][y]!=' '){
-      printf("\t\t\t\tInvalid Move!!\n");
-    }
-    else{
-      board[x][y] = PLAYER2;
-      break;
-    }
-  } while (board[x][y]!= ' ');
 }
 
 void computerEasyMove(){
   int x;
   int y;
+  static unsigned long firstSeed = 0x5AA5F100;
+  static unsigned long secondSeed = 0x5AA5F700;
 
   if(checkSpaces() > 0){
     do{
-      x = rand() % 3;
-      y = rand() % 3;
+      firstSeed = lfsr32(firstSeed);
+      secondSeed = lfsr32(secondSeed);
+      x = firstSeed % 3;
+      y = secondSeed % 3;
     }while(board[x][y]!=' ');
     board[x][y] = COMPUTER;
   }
@@ -332,7 +311,6 @@ void computerEasyMove(){
     printWinner(' ');
   }
 }
-
 
 void computerHardMove(){
   if(checkSpaces()> 0){
@@ -352,7 +330,6 @@ void computerHardMove(){
         }
       }
 
-
       if(board[i][1] == COMPUTER && board[i][2] == COMPUTER){
         if(board[i][0] == ' '){
           board[i][0] = COMPUTER;
@@ -366,7 +343,6 @@ void computerHardMove(){
         }
       }
 
-
       if(board[i][0] == COMPUTER && board[i][2] == COMPUTER){
         if(board[i][1] == ' '){
           board[i][1] = COMPUTER;
@@ -379,7 +355,6 @@ void computerHardMove(){
           return;
         }
       }
-
 
       //rows check
       if(board[0][i] == COMPUTER && board[1][i] == COMPUTER){
@@ -395,7 +370,6 @@ void computerHardMove(){
         }
       }
 
-
       if(board[1][i] == COMPUTER && board[2][i] == COMPUTER){
         if(board[0][i] == ' '){
           board[0][i] = COMPUTER;
@@ -409,7 +383,6 @@ void computerHardMove(){
         }
       }
 
-
       if(board[0][i] == COMPUTER && board[2][i] == COMPUTER){
         if(board[1][i] == ' '){
           board[1][i] = COMPUTER;
@@ -422,7 +395,6 @@ void computerHardMove(){
           return;
         }
       }
-
     }
 
     //diagonals check
@@ -448,7 +420,6 @@ void computerHardMove(){
       }
     }
 
-
     if(board[1][1] == COMPUTER && board[2][2] == COMPUTER){
       if(board[0][0] == ' '){
         board[0][0] = COMPUTER;
@@ -469,7 +440,6 @@ void computerHardMove(){
         return;
       }
     }
-
 
     if(board[0][0] == COMPUTER && board[2][2] == COMPUTER){
       if(board[1][1] == ' '){
@@ -492,7 +462,6 @@ void computerHardMove(){
       }
     }
 
-
     //second diag
     if(board[0][2] == COMPUTER && board[1][1] == COMPUTER){
       if(board[2][0] == ' '){
@@ -507,7 +476,6 @@ void computerHardMove(){
       }
     }
 
-
     if(board[1][1] == COMPUTER && board[2][0] == COMPUTER){
       if(board[0][2] == ' '){
         board[0][2] = COMPUTER;
@@ -520,7 +488,6 @@ void computerHardMove(){
         return;
       }
     }
-
 
     if(board[0][2] == COMPUTER && board[2][0] == COMPUTER){
       if(board[1][1] == ' '){
@@ -542,7 +509,6 @@ void computerHardMove(){
         return;
       }
     }
-
 
     if(board[0][1] == PLAYER|| board[1][2] == PLAYER || board [2][1] == PLAYER || board[1][0] || board[1][1]){ //maybe leave board[1][1] there
       computerEasyMove();
@@ -577,7 +543,6 @@ void computerHardMove(){
   }
 }
 
-
 char checkWinner(){
   int i;
   for(i = 0; i < 3; i++){ //column checks
@@ -600,14 +565,19 @@ char checkWinner(){
 }
 
 void printWinner(char winner){
-  if(winner == PLAYER){
+if(winner == PLAYER){
     printf("\t\t\t************************************\n");
-    printf("\t\t\t\tYOU WIN!\n\t\t\t\tCONGRATULATIONS!!\n");
+    printf("\t\t\t\tPLAYER X WINS!!\n");
+    printf("\t\t\t************************************\n");
+  }
+  else if(winner == PLAYER2){
+    printf("\t\t\t************************************\n");
+    printf("\t\t\t\tPLAYER O WINS!!\n");
     printf("\t\t\t************************************\n");
   }
   else if(winner == COMPUTER){
     printf("\t\t\t************************************\n");
-    printf("\t\t\t\tYOU LOSE :(\n\t\t\t\tBETTER LUCK NEXT TIME :)\n");
+    printf("\t\t\t\tCOMPUTER WINS!! BETTER LUCK NEXT TIME!\n");
     printf("\t\t\t************************************\n");
   }
   else {
@@ -617,20 +587,36 @@ void printWinner(char winner){
   }
 }
 
-void print2PlayerWinner(char winner){
-  if(winner == PLAYER){
-    printf("\t\t\t************************************\n");
-    printf("\t\t\t\tXPLAYER X WINS!!\n");
-    printf("\t\t\t************************************\n");
+void addHistory(char winner[], char loser[]){
+  FILE* filePtr = fopen("history.txt", "a");
+
+  if (filePtr == NULL){
+    printf("Error opening the file history.txt\n");
+    return;
   }
-  else if(winner == PLAYER2){
-    printf("\t\t\t************************************\n");
-    printf("\t\t\t\tPLAYER O WINS!!\n");
-    printf("\t\t\t************************************\n");
+  if(checkWinner() == ' '){
+    fprintf(filePtr,"%s and %s had a tie!\n", winner, loser);
   }
-  else {
-    printf("\t\t\t************************************\n");
-    printf("\t\t\t\tIT'S A TIE :|\n");
-    printf("\t\t\t************************************\n");
-  }
+  fprintf(filePtr,"%s won against %s\n", winner, loser);
+  fclose(filePtr);
+}
+
+unsigned long lfsr32(unsigned long seed){
+    //if seed is equal 0 it will return the next 32-bit value
+    if(seed == 0){
+        return seed + 1;
+    }
+    //creating new bit
+    unsigned long new_bit;
+    // using for loop to generate 32 new bits
+    for(int i = 0; i< 32; i++){//calculating new bit
+    //feedback polinomial: x^32 + x^30 + x^26 + x^25 + 1
+    //               /tap 32/      /tap 30/      /tap 26/      /tap 25/
+        new_bit = ( (seed >> 0) ^ (seed >> 2) ^ (seed >> 6) ^ (seed >> 7) ) & 1;
+        //modifying seed
+        //shifting seed to the right and putting new bit at the front shifted by 31-bit
+        seed = (seed >> 1) | (new_bit << 31);
+    }
+    //returning seed
+    return seed;
 }
